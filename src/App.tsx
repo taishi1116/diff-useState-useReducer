844ed6af853d07a1334a10dd9c8073075148f031
tis1116 <shinoda1116.miwa@gmail.com>
2021-09-30 19:18:00 +0900

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,17 @@
-import { EditNotificationDestination } from "./components/UseReducerPractice/EditNotificationDestination";
-import { ViewNotificationDestination } from "./components/UseReducerPractice/ViewNotificationDestination";
-import { useReducer } from "react";
-import { reducer } from "./components/UseReducerPractice/reducer";
-
-export type DisplayMode = "view" | "edit";
-export type NotificationDestination = {
-  displayMode: DisplayMode;
-  email: string;
-};
-
-const initialState: NotificationDestination[] = [];
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
-  const [notifications, dispatch] = useReducer(reducer, initialState);
-
-  const isNotificationFillMaximum = notifications.length <= 5;
   return (
-    <div className="App">
-      {notifications.map((o, mIndex) => (
-        <>
-          {o.displayMode === "edit" ? (
-            <EditNotificationDestination
-              index={mIndex + 1}
-              value={o.email}
-              onChange={(e) =>
-                dispatch({
-                  type: "updateNotification",
-                  payload: { index: mIndex, value: e.target.value },
-                })
-              }
-              onDelete={() =>
-                dispatch({
-                  type: "deleteNotification",
-                  payload: { index: mIndex },
-                })
-              }
-              onSave={() =>
-                dispatch({
-                  type: "changeDisplayMode",
-                  payload: { index: mIndex, displayMode: "view" },
-                })
-              }
-            />
-          ) : (
-            <ViewNotificationDestination
-              value={o.email}
-              onClick={() =>
-                dispatch({
-                  type: "changeDisplayMode",
-                  payload: { index: mIndex, displayMode: "view" },
-                })
-              }
-            />
-          )}
-        </>
-      ))}
-
-      {isNotificationFillMaximum ? (
-        <button onClick={() => dispatch({ type: "newNotification" })}>
-          追加
-        </button>
-      ) : null}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Link to="/useReducer">useReducer</Link>
+          <Link to="/useState">useState</Link>
+        </Route>
+        <Route path="/useReducer"></Route>
+        <Route path="/userState"></Route>
+      </Switch>
+    </Router>
   );
 }
---
+++ src/pages/UseReducer.tsx
@@ -0,0 +1,66 @@
+import { EditNotificationDestination } from "../components/UseReducerPractice/EditNotificationDestination";
+import { ViewNotificationDestination } from "../components/UseReducerPractice/ViewNotificationDestination";
+import { useReducer } from "react";
+import { reducer } from "../components/UseReducerPractice/reducer";
+
+export type DisplayMode = "view" | "edit";
+export type NotificationDestination = {
+  displayMode: DisplayMode;
+  email: string;
+};
+
+const initialState: NotificationDestination[] = [];
+
+export const UseReducer = () => {
+  const [notifications, dispatch] = useReducer(reducer, initialState);
+
+  const isNotificationFillMaximum = notifications.length <= 5;
+  return (
+    <div className="App">
+      {notifications.map((o, mIndex) => (
+        <>
+          {o.displayMode === "edit" ? (
+            <EditNotificationDestination
+              index={mIndex + 1}
+              value={o.email}
+              onChange={(e) =>
+                dispatch({
+                  type: "updateNotification",
+                  payload: { index: mIndex, value: e.target.value },
+                })
+              }
+              onDelete={() =>
+                dispatch({
+                  type: "deleteNotification",
+                  payload: { index: mIndex },
+                })
+              }
+              onSave={() =>
+                dispatch({
+                  type: "changeDisplayMode",
+                  payload: { index: mIndex, displayMode: "view" },
+                })
+              }
+            />
+          ) : (
+            <ViewNotificationDestination
+              value={o.email}
+              onClick={() =>
+                dispatch({
+                  type: "changeDisplayMode",
+                  payload: { index: mIndex, displayMode: "view" },
+                })
+              }
+            />
+          )}
+        </>
+      ))}
+
+      {isNotificationFillMaximum ? (
+        <button onClick={() => dispatch({ type: "newNotification" })}>
+          追加
+        </button>
+      ) : null}
+    </div>
+  );
+};
